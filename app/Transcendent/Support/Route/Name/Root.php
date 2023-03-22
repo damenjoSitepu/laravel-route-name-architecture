61e109a79a3eacb759dfec59bcdfafc2815181a1
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-03-22 17:13:08 +0700

    Update: Remove comment in root.php

--- app/Transcendent/Support/Route/Name/Root.php
+++ app/Transcendent/Support/Route/Name/Root.php
@@ -4,7 +4,6 @@ namespace App\Transcendent\Support\Route\Name;
 
 // Contracts / Interface
 use App\Transcendent\Contracts\RouteRootName;
-use App\Transcendent\Contracts\RouteRootNameValidation;
 // Route Name Modules
 use App\Transcendent\Support\Route\Name\Modules\Home;
 use App\Transcendent\Support\Route\Name\Modules\Profile;
@@ -58,22 +57,5 @@ class Root implements RouteRootName {
             'firstPath'     => $firstPath,
             'secondPath'    => $secondPath
         ]);
-
-
-
-
-        // // If Only First Name Not Empty
-        // if (!empty($firstPath) && empty($secondPath)) {
-        //     if (! isset(self::$allRouteName[$firstPath])) {
-        //         return self::$allRouteName['HOME']['INDEX'];
-        //     }
-        //     return self::$allRouteName[$firstPath]['INDEX'];
-        // }
-        // // If Both Of Them Are Not Empty, Do Another Logic
-        // $defaultPath = self::$allRouteName[$firstPath];
-        // $firstPath = (isset($defaultPath)) ? $firstPath : 'HOME';
-        // $secondPath = (isset($defaultPath[$secondPath])) ? $secondPath : 'INDEX';
-
-        // return self::$allRouteName[$firstPath][$secondPath];
     }
 }
